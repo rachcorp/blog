ffb90a8593b92137150e3c45eff87c823c83a6ce
Osman <osman@Osmans-MacBook-Pro.local>
2025-07-13 23:36:12 -0400

    updated the blog hero size

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'CloudAgent.io Blog';
-export const SITE_DESCRIPTION = 'Insights on cloud security, automation, and modern infrastructure from the CloudAgent.io team.';
+export const SITE_DESCRIPTION = 'Thoughts on cloud security, automation, and modern infrastructure from the CloudAgent.io team.';
